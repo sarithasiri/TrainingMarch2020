631cab7f9546d3aad31f136eef71e6c3c852fbec
sarithasiri <39800727+sarithasiri@users.noreply.github.com>
2020-04-11 21:33:55 +0530

    Second commit
    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Welomc to Sample Clinet server application");
+		System.out.println("Second Statement");
  //jar -cvfm SampleApps.jar manifest.txt com/test/*.class
 	}
 }
