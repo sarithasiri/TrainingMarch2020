199e18dd91582174abceb15eeec8c4931bc648b1
sarithasiri <39800727+sarithasiri@users.noreply.github.com>
2020-04-11 21:30:57 +0530

    Version2 commited
    Version2 commited

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.test;
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welomc to Sample Clinet server application");
+ //jar -cvfm SampleApps.jar manifest.txt com/test/*.class
+	}
+}
